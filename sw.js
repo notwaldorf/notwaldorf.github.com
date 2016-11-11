dee7bd5e640b334188b06a3499bc298b92c4c205
Monica Dinculescu <noms@google.com>
2016-11-11 15:57:05 -0800

    add a service worker?

---
+++ sw.js
@@ -0,0 +1,50 @@
+var VERSION = '0.0.1';
+
+this.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(VERSION).then(cache => {
+    return cache.addAll([
+      '/',
+      '/index.html',
+      '/about',
+      '/talks',
+      '/css/base.css',
+      '/css/mobile.css',
+      '/css/pygments.css',
+      '/manifest.json',
+      '/images/favicon.png',
+      '/images/avatar.jpeg',
+      '/elements/emoji-rain.html',
+      '/bower_components/webcomponentsjs/webcomponents-lite.js',
+    ]).then(_ => this.skipWaiting());
+}))});
+
+this.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then((res) => {
+    // If there is no match in the cache, we get undefined back,
+    // in that case go to the network!
+    return res ? res : handleNoCacheMatch(e);
+  }));
+});
+
+this.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then((keys) => {
+    return Promise.all(keys.map(k => {
+      if (k !== VERSION) {
+        return caches.delete(k);
+      }
+    })).then(_ => {
+      return this.clients.claim()
+    });
+}))});
+
+// fetch from network
+// and put into our cache
+function handleNoCacheMatch(e) {
+  return fetch(e.request).then(res => {
+    return caches.open(VERSION).then(cache => {
+      cache.put(e.request, res.clone());
+
+      return res;
+    });
+  });
+}
