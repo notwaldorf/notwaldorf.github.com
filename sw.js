f430519bcde00f8b2eb750060432c708b6cb7abe
Monica Dinculescu <noms@google.com>
2018-02-14 17:58:40 -0800

    do we think this service worker works? eh? eh?

--- sw.js
+++ sw.js
@@ -1,58 +1,65 @@
-var VERSION = '0.0.3';
+var APP_PREFIX = 'MEOWNICA_';     // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = '0.0.1';                    // Version of the off-line cache (change this value everytime you want to update cache)
+var CACHE_NAME = APP_PREFIX + VERSION;
+var URLS = [                            // Add URL you want to cache in this list.
+  '/',
+  '/index.html',
+  '/css/base.css',
+  '/css/mobile.css',
+  '/css/pygments.css',
+  '/manifest.json',
+  '/images/favicon.png',
+  '/images/avatar.jpeg',
+  '/elements/emoji-rain.html',
+  '/bower_components/webcomponentsjs/webcomponents-lite.js',
+]
 
-this.addEventListener('install', function(e) {
-  e.waitUntil(caches.open(VERSION).then(cache => {
-    return cache.addAll([
-      '/',
-      '/index.html',
-      '/css/base.css',
-      '/css/mobile.css',
-      '/css/pygments.css',
-      '/manifest.json',
-      '/images/favicon.png',
-      '/images/avatar.jpeg',
-      '/elements/emoji-rain.html',
-      '/bower_components/webcomponentsjs/webcomponents-lite.js',
-    ]).then(_ => this.skipWaiting());
-}))});
+var debug = false;
 
-this.addEventListener('fetch', function(e) {
-  e.respondWith(caches.match(e.request).then((res) => {
-    // If there is no match in the cache, we get undefined back,
-    // in that case go to the network!
-    return res ? res : fetch(e.request);
-  }));
-});
-
-this.addEventListener('activate', function(e) {
-  e.waitUntil(caches.keys().then((keys) => {
-    return Promise.all(keys.map(k => {
-      if (k !== VERSION) {
-        return caches.delete(k);
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  if (debug) console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        if (debug) console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        if (debug) console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
       }
-    })).then(_ => {
-      return this.clients.claim()
-    });
-}))});
+    })
+  )
+})
 
-// fetch from network
-// and put into our cache
-function handleNoCacheMatch(e) {
-  return fetch(e.request).then(res => {
-    var origin = location.origin;
-    var weCare =
-        res.url.indexOf(origin + '/images') !== -1 ||
-        res.url.indexOf(origin + '/fonts') !== -1 ||
-        res.url.indexOf(origin + '/about') !== -1 ||
-        res.url.indexOf(origin + '/talks') !== -1 ||
-        res.url.indexOf(origin + '/posts') !== -1;
+// Cache resources
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      if (debug) console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(URLS)
+    })
+  )
+})
 
-    if (!weCare)
-      return res;
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create white list
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_NAME)
 
-    return caches.open(VERSION).then(cache => {
-      cache.put(e.request, res.clone());
-      return res;
-    });
-  });
-}
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          if (debug) console.log('deleting cache : ' + keyList[i] )
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
